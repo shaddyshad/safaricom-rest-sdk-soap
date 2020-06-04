8cb22d3bd27e265b83d7787cfe9ab5d0efc87ff2
shaddyshad <mwangishadrack5@gmail.com>
2020-06-04 19:45:32 +0300

    add b2c test

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config
+const {b2c} = require('./payments')
+
+// make a b2c
+const tx = {
+    amount: 100,
+    idNum: '2187213',
+    recipient: '34300327',
+    commandId: 1
+}
+
+b2c.initiateB2C(tx)
