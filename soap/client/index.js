2b095ece72b70f097e38ba3761d97581dd58c919
shaddyshad <mwangishadrack5@gmail.com>
2020-06-05 18:06:16 +0300

    add axios api interface

--- soap/client/index.js
+++ soap/client/index.js
@@ -1,12 +1,17 @@
 const xmljs = require('xml-js');
 const makeConverter = require('./converter')
+const axios = require('axios')
 
 const json2Soap = makeConverter(xmljs);
 
+const PROVIDER_URL = process.env.PROVIDER_URL;
 
-module.exports = Object.freeze({
-        postB2C
+// create an axios instance
+const api = axios.create({
+    baseUrl: PROVIDER_URL,
+    timeout: 1000
 })
+
 
 /**
  * Create a b2c soap request
@@ -14,7 +19,18 @@ module.exports = Object.freeze({
 async function postB2C(transaction){
     const body = json2Soap(transaction)
 
-    console.log(body)
+    // header
+    const headers ={
+        'Content-Type': 'application/xml'
+    }
 
-    return body;
+    return api.post('/mminterface/request', headers, body)
+        .then(console.log)
+        .catch(console.log)
 }
+
+
+
+module.exports = Object.freeze({
+    postB2C
+})
