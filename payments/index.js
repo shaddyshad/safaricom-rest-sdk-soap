3181287d32ab26b991866bbe371c0c31e88f41f7
rawplutonium <lgithigi@gmail.com>
2020-06-09 17:02:28 +0300

    base 64 encode

--- payments/index.js
+++ payments/index.js
@@ -30,7 +30,7 @@ function getCurrentTime(){
  */
 function encodePassword(spid, pass, timestamp){
     // use base64(sha256(spid + pass + timestamp))
-    return btoa(sha256(spid + pass + timestamp))
+    return Buffer.from((sha256(spid + pass + timestamp))).toString('base64');
 }
 
 function encodeInitiatorPassword(InitiatorPassword){
